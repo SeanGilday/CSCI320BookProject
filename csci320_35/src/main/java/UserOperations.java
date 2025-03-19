07e9e6a121c40c6fc3898737ee941a7504138626
Sean Gilday <spgilday19@gmail.com>
2025-03-19 14:15:36 -0400

    Changed menu wording

--- csci320_35/src/main/java/UserOperations.java
+++ csci320_35/src/main/java/UserOperations.java
@@ -127,8 +127,8 @@ public class UserOperations {
      */
     private static boolean showMainMenu() {
         System.out.println(
-                "Would you like to\n\t(1) Create a collection\n\t(2) Modify a collection\n\t" +
-                        "(3) See all collections\n\t(4) Search for a book\n\t" +
+                "Would you like to\n\t(1) Create a collection\n\t(2) Modify one of your collections\n\t" +
+                        "(3) See all your collections\n\t(4) Search for a book\n\t" +
                         "(5) Read a book\n\t(6) Rate a book\n\t" +
                         "(7) Follow another user\n\t(8) Unfollow another user\n\t(9) Exit");
         int mainChoice = getUserChoice(9);
